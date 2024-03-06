76a6054f7c0f482d68ec5fdd390ec4165d12e510
Tsewanghyolmo01 <tds22@njit.edu>
2024-03-05 19:33:18 -0500

    added video for demo

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Buttons from './components/reusable/MenuButton';
+import animationVid from './Animation.mp4';
+
+
 
 function App() {
   return (
-    <>    <Buttons></Buttons>
+    <>   
+      <div className='Stats_container'>
+         <Buttons></Buttons>
+        <div className='TemporaryVid'>
+        
+           <video src={animationVid} className='TemporaryVid' preload='auto' controls autoPlay loop />
+        </div>
+
+      </div>
     </>
   );
 }
